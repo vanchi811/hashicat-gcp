ec0691a6027441f4248a22f6b0299449886049f6
vanchi811 <iamchinmay.com@gmail.com>
2022-06-18 19:35:29 +0000

    added networkl

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
 
+module "network" {
+    source  = "app.terraform.io/chinmay-test/network/google"
+    version = "3.4.0"
     project_id   = var.project
     network_name = "gaurav-network"
     routing_mode = "GLOBAL"
